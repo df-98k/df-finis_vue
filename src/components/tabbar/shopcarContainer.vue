9fca32cc1e8c3a4b109382b0be20c6f709989332
dufeng <2517838529@qq.com>
2019-07-30 08:59:31 +0800

    vue项目完成

--- src/components/tabbar/shopcarContainer.vue
+++ src/components/tabbar/shopcarContainer.vue
@@ -2,20 +2,25 @@
   <div class="shopcar-container">
     <!-- 商品列表区域 -->
     <div class="goods-list">
-      <div class="mui-card" v-for="item in goodslist" :key="item.id">
+      <div class="mui-card" v-for="(item,i) in goodslist" :key="item.id">
         <div class="mui-card-content">
-          <div class="mui-card-content-inner">
+          <div class="mui-card-content-inner jiesuan">
             <!-- 开关按钮 -->
-           <mt-switch></mt-switch>
+           <mt-switch
+           v-model="$store.getters.getGoodsSelected[item.id]"
+            @change="selectedChanged(item.id, $store.getters.getGoodsSelected[item.id])"
+           ></mt-switch>
 
             <img :src="item.thumb_path" alt />
 
             <div class="info">
               <h3>{{item.title}}  </h3>
               <p>
+
                 <span>￥{{item.sell_price}} </span>
-                <number></number>
-                <a href="#">删除</a>
+                <!-- :initcount="$store.getters.getGoodsCount[item.id]" --> 
+                <number :initcount="$store.getters.getGoodsCound[item.id]" :goodsid="item.id"></number>
+                <a href="#" @click.prevent="remove(item.id,i)">删除</a>
               </p>
             </div>
           </div>
@@ -28,7 +33,16 @@
     <!-- 商品结算区域 -->
     <div class="mui-card">
       <div class="mui-card-content">
-        <div class="mui-card-content-inner">这是一个最简比如一篇文章的预览图、作者信息、点赞数量等</div>
+        <div class="mui-card-content-inner">
+            <div >
+
+               <p class="allprice">结算清单:</p>
+              <p>已勾选商品 <span class="red">  {{$store.getters.getAllgoodsPlay.number}} </span>件</p>
+              <p>总价<span>￥  {{$store.getters.getAllgoodsPlay.price}} </span></p>
+
+            </div>
+           <mt-button type="danger" class="frigt">去结算</mt-button> 
+        </div>
       </div>
     </div>
   </div>
@@ -66,6 +80,16 @@ export default {
             this.goodslist = result.body.message;
           }
         });
+    },
+    remove(id,index){
+      
+      // 删除两个，一是store中的ID，二是组件中的 goodslist 中，对应要删除的那个商品，使用 index 来删除
+      this.goodslist.splice(index,1)//splice 从第i开始删除一个
+      this.$store.commit("removeFormCar",id)
+    },
+    selectedChanged(id,val){
+        // 每当点击开关，把最新的 快关状态，同步到 store 中
+      this.$store.commit("updateGoodsSelected",{id,selected:val})
     }
   },
   components: {
@@ -95,5 +119,18 @@ span {
   align-items: center;
 
 };
+
+  .red {
+      color: red;
+      font-weight: bold;
+      font-size: 16px;
+    }
+.frigt{
+  margin-left: 80px;
+  
+}
+.allprice{
+  font-size: 20px
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -85,6 +85,36 @@ var store = new Vuex.Store({
       // 当 更新 car 之后，把 car 数组，存储到 本地的 localStorage 中
 
       localStorage.setItem('car', JSON.stringify(state.car))
+    },
+    updateGoodsInfo(state,goodsinfo){
+      state.car.some(item=>{
+        if(item.id==goodsinfo.id){
+          item.count=parseInt(goodsinfo.count)
+          return true
+        }
+      })
+      // 购车内修改商品的数量。本地存储也会改变
+      localStorage.setItem('car', JSON.stringify(state.car))
+    },
+    removeFormCar(state,id){
+       // 根据Id，从store 中的购物车中删除对应的那条商品数据
+      state.car.some((item,i)=>{
+        if(item.id==id){
+          state.car.splice(i,1)
+          return true;
+        }
+      })
+        // 将删除完毕后的，最新的购物车数据，同步到 本地存储中
+        localStorage.setItem('car', JSON.stringify(state.car))
+    },
+    updateGoodsSelected(state,info){
+      state.car.some(item=>{
+        if(item.id==info.id){
+          item.selected=info.selected
+        }
+      })
+        // 把最新的 所有购物车商品的状态保存到 store 中去
+        localStorage.setItem('car', JSON.stringify(state.car))
     }
 
   },
@@ -96,8 +126,40 @@ var store = new Vuex.Store({
       });
       return c
 
+    },
+    getGoodsCound(state){
+      var x={}
+      state.car.forEach(item=>{
+       x[item.id]=item.count
+      })
+      return x
+  
+    },
+    getGoodsSelected(state){
+      var f={}
+      state.car.forEach(item=>{
+       f[item.id]=item.selected
+      })
+      return f
+    },
+    // 结算区域的操作
+    getAllgoodsPlay(state){
+      var z ={
+        number:0,//勾选的总数量
+        price:0,//勾选的总价
+      }
+      state.car.forEach(item=>{
+        if(item.selected){
+           z.number+=item.count
+           z.price+=item.price*item.count
+        }
+      })
+      return z
     }
-  }
+
+  },
+
+
 
 })
 
